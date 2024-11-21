98a1a89ee0e1cd0951c485932062eaaaee8dc343
Aadistian <adistian59@gmail.com>
2024-11-21 10:06:49 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,12 @@ const nextConfig = {
   compiler: {
     styledComponents: true, // Aktifkan jika menggunakan styled-components
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.cache = false;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,25 @@
-// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-    return (
-        <Html>
-            <Head>
-                <meta
-                    name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no"
-                />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Preload Google Fonts */}
+        <link
+          rel="preload"
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&display=swap"
+          as="style"
+        />
+        {/* Load Google Fonts */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
