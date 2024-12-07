596466d017a5f3d9aca25ebd8be53f1cd765ac95
Aadistian <adistian59@gmail.com>
2024-12-07 16:26:19 +0700

    update nommor

--- src/app/components/homeButton/headerButton.tsx
+++ src/app/components/homeButton/headerButton.tsx
@@ -4,18 +4,21 @@ import React, { useState } from "react";
 import Image from "next/image";
 import styles from "./headerButton.module.css";
 import arrow from "/public/homeAsset/arrow.svg";
+import Link from "next/link";
 
 export default function HeaderButton() {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div
-      className={`${styles.headerButton} ${isHovered ? styles.hovered : ""}`}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <p className={styles.title}>Pesan Sekarang</p>
-      <Image className={styles.arrow} src={arrow} alt="arrow" />
-    </div>
+    <Link href="https://wa.me/628117212566" target="blank">
+      <div
+        className={`${styles.headerButton} ${isHovered ? styles.hovered : ""}`}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <p className={styles.title}>Pesan Sekarang</p>
+        <Image className={styles.arrow} src={arrow} alt="arrow" />
+      </div>
+    </Link>
   );
 }
